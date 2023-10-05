8cf325c7a211ce54b6cb975f5e7c6791741c6c37
Federico Eigler <fedeeigler@hotmail.com>
2023-10-05 16:54:01 -0300

    SO_&_Exp
    -Cuando derrotamos al jefe final o a los zombies que este genera, ganamos experiencia.
    -Se terminaron de pasar las configuración del inspector al SO del jugador.
    -Se arreglo un error en la muerte del jefe final (Ahora tiene animaciones y lanza partículas al morir).

--- Platformer_Unity_PV2/Assets/Scripts/PerfilJugador.cs
+++ Platformer_Unity_PV2/Assets/Scripts/PerfilJugador.cs
@@ -5,25 +5,44 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "NuevoPerfilJugador", menuName = "SO/Perfil Jugador" )]
 public class PerfilJugador : ScriptableObject
 {
-    //[SerializeField]
+
+
     [Header("Movimiento Horizontal")]
-   // [Header("Movimiento Horizontal")]
     [SerializeField] float WalkSpeed = 5f;
     public float WalkSpeed1 { get => WalkSpeed; set => WalkSpeed = value; }
-
     [SerializeField] float RunSpeed = 10f;
     public float RunSpeed1 { get => RunSpeed; set => RunSpeed = value; }
 
-    //[SerializeField]
-    //[Tooltip("Experiencia y Nivel")] 
-    //[Header("Experiencia y Nivel")]
-    private int Level;
-    public int level { get => Level; set => Level = value;}
-    private int xP;
-    public int XP { get => xP; set => xP = value; }
 
+
+    [Header("Jump")]
+    [SerializeField] private float fuerzaSalto = 5f;
+    public float FuerzaSalto { get => fuerzaSalto; set => fuerzaSalto = value; }
+    [SerializeField] private AudioClip JumpSFX;
+    public AudioClip JumpSFX1 { get => JumpSFX; set => JumpSFX = value; }
+    [SerializeField] private AudioClip CollisionSFX;
+    public AudioClip CollisionSFX1 { get => CollisionSFX; set => CollisionSFX = value; }
+
+
+
+
+    [Header("Lifes")]
+    [SerializeField] private float vida = 5f;
+    public float Vida { get => vida; set => vida = value; }
+    [SerializeField] private float InmortalTime;
+    public float InmortalTime1 { get => InmortalTime; set => InmortalTime = value; }
+    [SerializeField] private  float intervaloParpadeo = 0.5f;
+    public float IntervaloParpadeo { get => intervaloParpadeo; set => intervaloParpadeo = value; }
+
+
+
+    [Header("Progresion")]
     [SerializeField] private int xPNextLevel;
     public int XPNextLevel { get => xPNextLevel; set => xPNextLevel = value; }
     [SerializeField] private int scaleXp;
     public int ScaleXp { get => scaleXp; set => scaleXp = value; }
+    private int Level;
+    public int level { get => Level; set => Level = value;}
+    private int xP;
+    public int XP { get => xP; set => xP = value; }
 }
--- Platformer_Unity_PV2/Assets/Scripts/Progression.cs
+++ Platformer_Unity_PV2/Assets/Scripts/Progression.cs
@@ -21,16 +21,6 @@ void OnEnable()
         perfilJugador.level++;
         perfilJugador.XP -= perfilJugador.XPNextLevel;
         perfilJugador.XPNextLevel += perfilJugador.ScaleXp;
-    }
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        Debug.Log("Nuevo nivel: " + perfilJugador.level);
     }
 }
